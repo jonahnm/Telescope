5bbd7be0ad93ece7a34eac7fdf872e8ec8434fa8
sora <jonahdb2@gmail.com>
2024-01-20 23:34:21 -0500

    Fix createbasebin.sh

--- TelescopeBin/createbasebin.sh
+++ TelescopeBin/createbasebin.sh
@@ -3,16 +3,17 @@ mkdir ./baseboin
 
 cd ./helloworld
 make
-cp .theos/obj/debug/arm64e/HelloWorld ../baseboin
+cp .theos/obj/debug/arm64e/HelloWorld ../baseboin/
+codesign -s - ../baseboin/HelloWorld
 cd -
 
 rm -rf "./basebin.tc"
 chmod +x TrustCache*
 arch=$(uname -m)
 if [[ $arch == arm* ]] || [[ $arch == aarch64 ]]; then
-./TrustCache create -v 2 "./basebin.tc" "./baseboin/"
+./TrustCache create -v 2 basebin.tc baseboin
 elif [[ $arch == x86_64* ]]; then
-./TrustCache_x86_64 create -v 2 "./basebin.tc" "./baseboin/"
+./TrustCache_x86_64 create -v 2 basebin.tc baseboin
 fi
 
 
